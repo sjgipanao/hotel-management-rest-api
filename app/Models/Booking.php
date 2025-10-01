86d681354213c92644aa9936a0eadd0a849bad2e
SophiaGipanao <gipanaosophia90@gmail.com>
2025-10-01 15:55:16 +0800

    created new migration and model for bookings

---
+++ app/Models/Booking.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'room_id',
+        'guest_name',
+        'payment_price',
+        'occupancy',
+    ];
+}
--- app/Models/Room.php
+++ app/Models/Room.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Room extends Model
 {
-    //
+    protected $fillable = [
+        'room_type',
+        'capacity',
+        'description',
+        'price',
+    ];
 }

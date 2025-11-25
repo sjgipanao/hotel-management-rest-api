d62db9e0a876bd6bdfdb3aa48fc6e78d0e904a32
SophiaGipanao <gipanaosophia90@gmail.com>
2025-11-25 22:16:27 +0800

    Add validation using Form Request

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoomController;
+use App\Http\Controllers\AuthController;
 
 //Test Route
 Route::get('/test', function(Request $request){
     return response()->json(['message' => 'API is working fine']);
 });
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 
 //Get all rooms
@@ -25,7 +32,3 @@ Route::put('/rooms/{id}', [App\Http\Controllers\RoomController::class, 'update']
 
 //Delete a room
 Route::delete('/rooms/{id}', [App\Http\Controllers\RoomController::class, 'destroy']);
-
-//Toggle room availability status
-Route::patch('/rooms/{id}/toggle-status', [App\Http\Controllers\RoomController::class, 'toggleRoomStatus']);
-
